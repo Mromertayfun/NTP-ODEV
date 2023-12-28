ab740d962905020fef3c7771da6dfd1ab01fb014
ömer tayfun koç <123269012+Mromertayfun@users.noreply.github.com>
2023-12-28 15:52:18 +0300

    NTP-ODEV
    NTP-ODEV

---
+++ odev/ODEV/frmgiris.Designer.cs
@@ -0,0 +1,24 @@
+﻿namespace Okulodev
+{
+    partial class frmgiris
+    {
+        private System.ComponentModel.IContainer components = null;
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_kaydet;
+        private System.Windows.Forms.TextBox txt_tc;
+        private System.Windows.Forms.TextBox txt_soyad;
+        private System.Windows.Forms.TextBox txt_ad;
+        private System.Windows.Forms.Label lbl_tc;
+        private System.Windows.Forms.Label lbl_soyad;
+        private System.Windows.Forms.Label lbl_ad;
+    }
+}
+
